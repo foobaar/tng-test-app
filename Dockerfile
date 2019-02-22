47e9a90c055f66ac8c9fda228f8918644bd2a7d4
root <root@au-initializer-tng-dev-au-initializer-tng-7f96d9b968-gtrm9>
2019-02-22 21:54:15 +0000

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+###
+# ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+# Autonomic Proprietary 1.0
+# ——————————————————————————————————————————————————————————————————————————————
+# Copyright (C) 2019 Autonomic, LLC - All rights reserved
+# ——————————————————————————————————————————————————————————————————————————————
+# Proprietary and confidential.
+# 
+# NOTICE:  All information contained herein is, and remains the property of
+# Autonomic, LLC and its suppliers, if any.  The intellectual and technical
+# concepts contained herein are proprietary to Autonomic, LLC and its suppliers
+# and may be covered by U.S. and Foreign Patents, patents in process, and are
+# protected by trade secret or copyright law. Dissemination of this information
+# or reproduction of this material is strictly forbidden unless prior written
+# permission is obtained from Autonomic, LLC.
+# 
+# Unauthorized copy of this file, via any medium is strictly prohibited.
+# ______________________________________________________________________________
+###
+
+FROM autonomic-docker.jfrog.io/au/oracle-jdk:8
+RUN mkdir -p /home/au
+WORKDIR /home/au
+COPY target/java-barebones-spring-template-exec.jar java-barebones-spring-template.jar
+CMD ["run", "java-barebones-spring-template.jar"]
---
+++ src/test/java/com/autonomic/javabarebonesspringtemplate/JavaBarebonesSpringTemplateApplicationIT.java
@@ -0,0 +1,36 @@
+/*-
+ * ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+ * Autonomic Proprietary 1.0
+ * ——————————————————————————————————————————————————————————————————————————————
+ * Copyright (C) 2019 Autonomic, LLC - All rights reserved
+ * ——————————————————————————————————————————————————————————————————————————————
+ * Proprietary and confidential.
+ * 
+ * NOTICE:  All information contained herein is, and remains the property of
+ * Autonomic, LLC and its suppliers, if any.  The intellectual and technical
+ * concepts contained herein are proprietary to Autonomic, LLC and its suppliers
+ * and may be covered by U.S. and Foreign Patents, patents in process, and are
+ * protected by trade secret or copyright law. Dissemination of this information
+ * or reproduction of this material is strictly forbidden unless prior written
+ * permission is obtained from Autonomic, LLC.
+ * 
+ * Unauthorized copy of this file, via any medium is strictly prohibited.
+ * ______________________________________________________________________________
+ */
+
+package com.autonomic.javabarebonesspringtemplate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaBarebonesSpringTemplateApplicationIT {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
